357081a1477c9c7fec245a8226756983d0968ef4
Burke <beatty.burke@gmail.com>
2021-06-08 20:05:58 -0400

    tableData and waitinglistData variables added

---
+++ data/tableData.js
@@ -0,0 +1,26 @@
+const tableData = [
+    {
+        name: 'Burke',
+        phoneNumber: '9192344322',
+        email: 'burke@gmail.com',
+        uniqueID: '54321'
+    },
+    {
+        name: 'Robert',
+        phoneNumber: '9192344321',
+        email: 'robert@gmail.com',
+        uniqueID: '54322'
+    },
+    {
+        name: 'Sara',
+        phoneNumber: '9192344320',
+        email: 'sara@gmail.com',
+        uniqueID: '54323'
+    },
+    {
+        name: 'Hamlet',
+        phoneNumber: '9192344325',
+        email: 'hamlet@gmail.com',
+        uniqueID: '54324'
+    },
+]
---
+++ data/waitinglistData.js
@@ -0,0 +1,26 @@
+const waitinglistData = [
+    {
+        name: 'Beatty',
+        phoneNumber: '9192344322',
+        email: 'beatty@gmail.com',
+        uniqueID: '64321'
+    },
+    {
+        name: 'Pupo',
+        phoneNumber: '9192344321',
+        email: 'pupo@gmail.com',
+        uniqueID: '64322'
+    },
+    {
+        name: 'Lown',
+        phoneNumber: '9192344320',
+        email: 'lown@gmail.com',
+        uniqueID: '64323'
+    },
+    {
+        name: 'Valladares',
+        phoneNumber: '9192344325',
+        email: 'valladares@gmail.com',
+        uniqueID: '64324'
+    },
+]
